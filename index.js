4851cfcc814ec2b7ab6c332cc4c76f891523a4f2
rxnvaz18 <54378927+rxnvaz18@users.noreply.github.com>
2023-11-08 17:45:43 -0800

    fixed commented out area of code in index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 // Require needed modules.
+
 require('dotenv').config()
+
 const express = require('express')
 
 // Initialize the app object.
+
 const app = express()
 
 // code for requiring use of places.js router
@@ -10,13 +13,16 @@ const app = express()
 app.use('/places', require('./controllers/places'))
 
 // Create a homepage route.
+
 app.get('/', (req, res) => {
-    // This gets sent to the client 
+
+    res.send('I Am Awake')}
+)    
+// This gets sent to the client 
     // (your web browser most likely!)
-    res.send('I Am Awake')
-})
 // Listen for connections.
+
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.status(404).send("<h1>404 Page</h1>")
 })
 app.listen(process.env.PORT) 
