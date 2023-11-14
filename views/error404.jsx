895458a8fdd1dce84cf77e5467b61361aba777c1
rxnvaz18 <54378927+rxnvaz18@users.noreply.github.com>
2023-11-13 19:19:41 -0800

    added images to homepage and error page

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,6 +7,12 @@ return (
         <main>
             <h1>404: PAGE NOT FOUND</h1>
             <p>Oops, sorry, we can't find this page!</p>
+            <div>
+                <img src="./images/pug.jpg" alt="small black pug with glasses"/>
+            </div>
+            <div>
+                Photo by <a href="AUTHOR_LINK">Charles Deluvio</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
         </main>
     </Def>
 )
