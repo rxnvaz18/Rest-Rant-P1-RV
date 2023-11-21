5c9b6a9511185ed778ef54110400f63e9e506dc2
rxnvaz18 <54378927+rxnvaz18@users.noreply.github.com>
2023-11-20 16:26:13 -0800

    updated form route

--- controllers/places.js
+++ controllers/places.js
@@ -1,47 +1,47 @@
 const router = require('express').Router()
 // more code to be added
-
+const places = require('../models/places.js')
 router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'Savory Haven',
-        city: 'Salt Lake City',
-        state: 'UT',
-        cuisines: 'Chinese',
-        pic: './images/chinese.jpg'
-      }, 
-      {
-        name: 'Urban Spice Grill',
-        city: 'Los Angeles',
-        state: 'CA',
-        cuisines: 'Burgers and Hot Dogs',
-        pic: './images/burgers.jpg'
-      },
-      {
-        name: 'Culinary Oasis',
-        city: 'Las Vegas',
-        state: 'NV',
-        cuisines: 'Plant-Based Mediterranean',
-        pic: './images/medlounge.jpg'
-      },
-      {
-        name: 'Fusion Bites Lounge',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Korean/Mexican Fusion',
-        pic: './images/kmex.jpg'
-      },
-      {
-        name: 'Tantalizing Tastes Tavern',
-        city: 'San Diego',
-        state: 'CA',
-        cuisines: 'English/Indian Fusion',
-        pic: './images/pub.jpg'
-      }
-    ]
+//     let places = [{
+//         name: 'Savory Haven',
+//         city: 'Salt Lake City',
+//         state: 'UT',
+//         cuisines: 'Chinese',
+//         pic: './images/chinese.jpg'
+//       }, 
+//       {
+//         name: 'Urban Spice Grill',
+//         city: 'Los Angeles',
+//         state: 'CA',
+//         cuisines: 'Burgers and Hot Dogs',
+//         pic: './images/burgers.jpg'
+//       },
+//       {
+//         name: 'Culinary Oasis',
+//         city: 'Las Vegas',
+//         state: 'NV',
+//         cuisines: 'Plant-Based Mediterranean',
+//         pic: './images/medlounge.jpg'
+//       },
+//       {
+//         name: 'Fusion Bites Lounge',
+//         city: 'Seattle',
+//         state: 'WA',
+//         cuisines: 'Korean/Mexican Fusion',
+//         pic: './images/kmex.jpg'
+//       },
+//       {
+//         name: 'Tantalizing Tastes Tavern',
+//         city: 'San Diego',
+//         state: 'CA',
+//         cuisines: 'English/Indian Fusion',
+//         pic: './images/pub.jpg'
+//       }
+//     ]
       
     res.render('places/index', {places})
 })
---
+++ models/places.js
@@ -0,0 +1,36 @@
+module.exports = [{
+    name: 'Savory Haven',
+    city: 'Salt Lake City',
+    state: 'UT',
+    cuisines: 'Chinese',
+    pic: './images/chinese.jpg'
+  }, 
+  {
+    name: 'Urban Spice Grill',
+    city: 'Los Angeles',
+    state: 'CA',
+    cuisines: 'Burgers and Hot Dogs',
+    pic: './images/burgers.jpg'
+  },
+  {
+    name: 'Culinary Oasis',
+    city: 'Las Vegas',
+    state: 'NV',
+    cuisines: 'Plant-Based Mediterranean',
+    pic: './images/medlounge.jpg'
+  },
+  {
+    name: 'Fusion Bites Lounge',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Korean/Mexican Fusion',
+    pic: './images/kmex.jpg'
+  },
+  {
+    name: 'Tantalizing Tastes Tavern',
+    city: 'San Diego',
+    state: 'CA',
+    cuisines: 'English/Indian Fusion',
+    pic: './images/pub.jpg'
+  }
+]
