dc54100c18b48cbfcd2ab06638bb9620b1dec433
rxnvaz18 <54378927+rxnvaz18@users.noreply.github.com>
2023-11-29 16:53:15 -0800

    show route not connected

--- controllers/places.js
+++ controllers/places.js
@@ -2,6 +2,23 @@ const router = require('express').Router()
 // more code to be added
 const places = require('../models/places.js')
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id] })
+  }
+  
+})
+
+
+
+
 router.get('/:id/edit', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -21,42 +38,42 @@ router.get('/new', (req, res) => {
 })
 
 router.get('/', (req, res) => {
-//     let places = [{
-//         name: 'Savory Haven',
-//         city: 'Salt Lake City',
-//         state: 'UT',
-//         cuisines: 'Chinese',
-//         pic: './images/chinese.jpg'
-//       }, 
-//       {
-//         name: 'Urban Spice Grill',
-//         city: 'Los Angeles',
-//         state: 'CA',
-//         cuisines: 'Burgers and Hot Dogs',
-//         pic: './images/burgers.jpg'
-//       },
-//       {
-//         name: 'Culinary Oasis',
-//         city: 'Las Vegas',
-//         state: 'NV',
-//         cuisines: 'Plant-Based Mediterranean',
-//         pic: './images/medlounge.jpg'
-//       },
-//       {
-//         name: 'Fusion Bites Lounge',
-//         city: 'Seattle',
-//         state: 'WA',
-//         cuisines: 'Korean/Mexican Fusion',
-//         pic: './images/kmex.jpg'
-//       },
-//       {
-//         name: 'Tantalizing Tastes Tavern',
-//         city: 'San Diego',
-//         state: 'CA',
-//         cuisines: 'English/Indian Fusion',
-//         pic: './images/pub.jpg'
-//       }
-//     ]
+    let places = [{
+        name: 'Savory Haven',
+        city: 'Salt Lake City',
+        state: 'UT',
+        cuisines: 'Chinese',
+        pic: './images/chinese.jpg'
+      }, 
+      {
+        name: 'Urban Spice Grill',
+        city: 'Los Angeles',
+        state: 'CA',
+        cuisines: 'Burgers and Hot Dogs',
+        pic: './images/burgers.jpg'
+      },
+      {
+        name: 'Culinary Oasis',
+        city: 'Las Vegas',
+        state: 'NV',
+        cuisines: 'Plant-Based Mediterranean',
+        pic: './images/medlounge.jpg'
+      },
+      {
+        name: 'Fusion Bites Lounge',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Korean/Mexican Fusion',
+        pic: './images/kmex.jpg'
+      },
+      {
+        name: 'Tantalizing Tastes Tavern',
+        city: 'San Diego',
+        state: 'CA',
+        cuisines: 'English/Indian Fusion',
+        pic: './images/pub.jpg'
+      }
+    ]
       
     res.render('places/index', {places})
 })
---
+++ views/places/show.jsx
@@ -0,0 +1,22 @@
+const React = require('react')
+const Def = require('../default')
+
+function show (data) {
+    return (
+        <Def>
+          <main>
+            <h1>{data.place.name}</h1>
+            <div>
+              <h2>Rating</h2>
+              <p>Currently unrated</p>
+            </div>
+            <div>
+              <h2>Comments</h2>
+              <p>No comments yet!</p>
+              </div>
+          </main>
+        </Def>
+    )
+}
+
+module.exports = show
