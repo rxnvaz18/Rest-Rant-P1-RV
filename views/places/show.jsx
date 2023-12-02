c4f84742a020689d2ebff8120d380db4c8563ccf
unknown <roxanvaz31@gmail.com>
2023-12-02 13:00:37 -0800

    editted and reformatted it

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,8 +5,18 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
+            <div className="row">
+              <div className="col-sm-6">
+            <img src={data.place.pic} alt={data.place.name}/>
+            </div>
+              <h3>Located in {data.place.city}, {data.place.state}</h3>
+              
+              <div className="col-sm-6">
+                <h2>Description</h2>
+                <h3>{data.place.showEstablished()}</h3>
+                <h4>Serving {data.place.cuisine}</h4>
+            </div></div>
+
             <div>
               <h2>Rating</h2>
               <p>Currently unrated</p>
@@ -15,6 +25,8 @@ function show (data) {
               <h2>Comments</h2>
               <p>No comments yet!</p>
               </div>
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
+            
               <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                  <button type="submit" className="btn btn-danger">
                         Delete
