b8a110c824a02b8bad460e5d1b9fd7aff4ef08ba
unknown <roxanvaz31@gmail.com>
2023-12-09 08:33:32 -0800

    added comment form

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -49,6 +49,30 @@ function show (data) {
                         Delete
                   </button>
               </form> 
+              <h2>Need to Rant or Rave about a place?</h2>
+            <form action={`/places/${data.place.id}/comment`} method="POST">
+              <div className="row">
+                <div className="form-group col-sm-12">
+                  <label htmlFor="content">Content</label>
+                  <textarea id="content" name="content" className="form-control"></textarea>
+                </div>
+              </div>
+              <div className="row">
+                <div className="form-group col-sm-4">
+                  <label htmlFor="author">Critic</label>
+                  <input id="author" name="author" className="form-control" />
+                </div>
+                <div className="form-group col-sm-4">
+                  <label htmlFor="stars">Star Rating</label>
+                  <input type="range" step="0.5" min="1" max="5" id="stars" name="stars" className="form-control" />
+                </div>
+                <div className="form-group col-sm-2">
+                  <label htmlFor="rant">Rant?</label>
+                  <input type="checkbox" id="rant" name="rant" className="form-control" />
+                </div>
+              </div>
+              <input type="submit" className="btn btn-primary" value="Add Comment" />
+            </form>
           </main>
         </Def>
     )
