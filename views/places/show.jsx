f458f57be16dff72c68f73820eea3fdd98547644
unknown <roxanvaz31@gmail.com>
2023-12-09 08:05:57 -0800

    comments are loading on page

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,23 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+  let comments = (
+    <h3 className='inactive'>No comments yet!</h3>
+  )
+  if (data.place.comments.length) {
+    comments = data.place.comments.map(c => {
+      return (
+        <div className='border'>
+          <h2 className='rant'>{c.rant ? 'Rant!' : 'Rave!'}</h2>
+            <h4>{c.content}</h4>
+              <h3>
+                <strong>- {c.author}</strong>
+              </h3>
+              <h4>Rating: {c.stars}</h4>
+        </div>
+      )
+    })
+  }
     return (
         <Def>
           <main>
@@ -23,7 +40,7 @@ function show (data) {
             </div>
             <div>
               <h2>Comments</h2>
-              <p>No comments yet!</p>
+              {comments}
               </div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
             
